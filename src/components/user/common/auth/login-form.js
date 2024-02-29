b33a0481e7be4d0d5345feb99fc94b54036d9e63
ad <makara3416@gmail.com>
2024-02-29 15:23:59 +0100

    6v

--- src/components/user/common/auth/login-form.js
+++ src/components/user/common/auth/login-form.js
@@ -12,7 +12,8 @@ import { useNavigate } from "react-router-dom";
 
  const LoginForm = () => {
   const [ loading,setLoading] =useState(false)
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   
 
   const initialValues = {
@@ -36,12 +37,14 @@ import { useNavigate } from "react-router-dom";
       secureLocalStorage.setItem("token", respAuth.data.token); // guvenlik icin. Burasi  respAuth.data standart token  ismi
       
       //Bundan sonra kullanici bilgilerini almaliyiz getUser ile, bunun hicbir parametresi yok backenden bakabiliriz
-      const respUser = await getUser(); //Artik  token siz calismicak hep token isticek. aut-heeader dan cekiyoruz
-      dispatch(loginSuccess(respUser.data));
+      const respUser = await getUser(); //Artik  token siz calismicak hep token isticek. aut-heeader dan cekiyoruz. Yani getUser i cagiriyoruz
+       console.log (respUser.data);//kullanici bilgisi bize geliyomu consolden bakalim. Geliyor artik bu bilgileri merkezi state yani  store klasorundeki auth a yerlestircez 
+      dispatch(loginSuccess(respUser.data)); // bize backen in göndermis oldugu  user objesini (kullanici bilgilerini) merkezi state loginSuccess üerinden göndercez
       
-      navigate("/");
+      navigate("/"); // login olduktan sonra  home sayfasina gidiyor 
 
     } catch (err) {
+      dispatch(loginFailed()); //hata olursa merkezi state bosaltcak
       toast(err.response.data.message, "error") //err.response.data.message, bu backend in hata mesaji ve  "error" de Sweet in hata mesaji
     } 
 
@@ -97,78 +100,3 @@ export default LoginForm;
 
 
 
-
-
-
-
-
-
-/* import React, { useState } from "react";
-import { useFormik } from "formik";
-import { Form, Button, Spinner } from "react-bootstrap";
-import * as Yup from "yup";
-import PasswordInput from "../../../common/password-input/password-input";
-import { getUser, login } from "../../../../api/user-service";
-import { toast } from "../../../../utils/functions/swal";
-import secureLocalStorage from "react-secure-storage";
-import { useDispatch } from "react-redux";
-import { loginFailed, loginSuccess } from "../../../../store/slices/auth-slice";
-import { useNavigate } from "react-router-dom";
-
-const LoginForm = () => {
- 
-  
-
-  const initialValues = {
-    email: "",
-    password: "",
-  };
-
-  const validationSchema = Yup.object({
-    email: Yup.string().email().required("Please enter your email"),
-    password: Yup.string().required("Please enter your password"),
-  });
-
-  const onSubmit = async (values) => {
-    console.log(values);
-   
-  };
-
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit,
-  });
-
-
-  return (
-    <Form noValidate onSubmit={formik.handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          {...formik.getFieldProps("email")}
-          isInvalid={formik.touched.email && formik.errors.email}
-          isValid={formik.touched.email && !formik.errors.email}
-        />
-        <Form.Control.Feedback type="invalid">
-          {formik.errors.email}
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Password</Form.Label>
-        <PasswordInput
-          {...formik.getFieldProps("password")}
-          isInvalid={formik.touched.password && formik.errors.password}
-          isValid={formik.touched.password && !formik.errors.password}
-          error={formik.errors.password}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit"  >
-      Login
-      </Button>
-    </Form>
-  );
-};
-
-export default LoginForm; */
