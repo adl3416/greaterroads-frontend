8ed1d421079635847b554389aaf1ef2256dbc9e7
ad <makara3416@gmail.com>
2024-03-05 09:35:00 +0100

    6v

--- src/components/user/about/rent-path/rent-path.js
+++ src/components/user/about/rent-path/rent-path.js
@@ -1,15 +1,46 @@
-
-import React from 'react'
-import SectionHeader from '../../common/section-header/section-header'
+import React from "react";
+import SectionHeader from "../../common/section-header/section-header";
+import Spacer from "../../../common/spacer/spacer";
+import { Col, Container, Row } from "react-bootstrap";
+import { MdLocalCarWash, MdCreditScore } from "react-icons/md";
+import { TbSteeringWheel } from "react-icons/tb";
+import "./rent-path.scss";
 
 const RentPath = () => {
   return (
     <section>
-        <SectionHeader 
-        title="Qick &amp: easy car rental"
-        subtitle="Plan your trip now" />
-    </section>
-  )
-}
+      <SectionHeader
+        title="Quick &amp: easy car rental"
+        subtitle="Plan your trip now"
+        desc=""
+      />
 
-export default RentPath
+      <Spacer height={50} />
+
+      <Container className="rent-path" >
+        <Row>
+          <Col md={4}>
+            <MdLocalCarWash />
+            <h2>Select car</h2>
+            <p>Select your best car among our lux, hygienic and cheap cars</p>
+          </Col>
+
+          <Col md={4}>
+            <MdCreditScore />
+            <h2>Pay for it</h2>
+            <p>You can pay by creadit card, paypall or cash</p>
+          </Col>
+          <Col md={4}>
+            <TbSteeringWheel />
+            <h2>Let's drive</h2>
+            <p>
+              You can drive your rental car until the return time you selected
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default RentPath;
--- src/pages/user/about-page.js
+++ src/pages/user/about-page.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import PageHeader from '../../components/user/common/page-header/page-header'
+import RentPath from '../../components/user/about/rent-path/rent-path'
+import Spacer from '../../components/common/spacer/spacer'
 
 const AboutPage  = () => {
   return (
-    <div> <PageHeader title="About Us"/> </div>
+    <div>
+       <PageHeader title="About Us"/> 
+       <Spacer />
+       <RentPath/>
+       </div>
   )
 }
 
