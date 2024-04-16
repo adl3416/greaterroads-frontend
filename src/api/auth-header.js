45b41c93f45613a6e9a9784704c901335050f064
adl <makara3416@gmail.com>
2024-04-16 10:08:13 +0200

    e

--- src/api/auth-header.js
+++ src/api/auth-header.js
@@ -4,8 +4,8 @@ const authHeader = () => {
   const token = secureLocalStorage.getItem("token"); // header objesini olusturmak icin:  getItem ile  ecureLocalStorage den tokene get ile aldim.
 
   let header = {};
-  if (token) { //eger token doluysa ozaman header i return ediyoruz.Bos sa bos döner
-    header = { Authorization: `Bearer ${token}` }; //bu obje busekilde olusturulur
+  if (token) { //eger token doluysa ozaman header i  return ediyoruz.Bos sa bos döner.
+    header = { Authorization: `Bearer ${token}` }; //bu obje  busekilde olusturulur
   }
 
   return header;
